dff854c41cc182a8525b62de7274dc045d661f37
Ash-x1 <ash91p@proton.me>
2026-04-08 22:52:48 +0100

    Create Events controller with appropriate methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "events/index"
+  get "events/show"
+  get "events/new"
+  get "events/create"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -13,4 +17,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root "events#index"
+  resources :events
 end
