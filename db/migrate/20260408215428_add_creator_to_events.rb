773c429b8a9f0efa6a561e001dc57ac01524be23
Ash-x1 <ash91p@proton.me>
2026-04-08 22:59:17 +0100

    Add creator to Events and edit the migration and edit the Event model

---
+++ db/migrate/20260408215428_add_creator_to_events.rb
@@ -0,0 +1,6 @@
+class AddCreatorToEvents < ActiveRecord::Migration[8.1]
+  def change
+    # add_reference :events, :creator, null: false, foreign_key: true
+    add_reference :events, :creator, foreign_key: { to_table: :users }
+  end
+end
